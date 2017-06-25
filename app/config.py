b3ac8be1f3da6d919d833ff3d2414a96c0c937df
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-06-25 17:41:28 +0530

    Adding Flask secret key.

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ import os
 
 class BaseConfig(object):
     PORT = 5000
+    SECRET_KEY = '\xd1\xc2#|e\xf4\x15\x9e\xcbk\x96l\xcdw\x87\xc9W\xa5\x82Y\xb0y\x99\x98'
 
 class LocalConfig(BaseConfig):
     DEBUG = True
