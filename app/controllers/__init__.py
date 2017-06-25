7c653153ba8725069463def1c07fe316d72e217f
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-06-25 11:25:51 +0530

    Adding view controller.

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,8 +1,9 @@
 from .static_content_handler import root_handler
+from .views import view_handler
 
 controllers = [
     ("/", root_handler),
-    #("/api", heartbeat_handler),
+    ("/view", view_handler),
 ]
 
 
