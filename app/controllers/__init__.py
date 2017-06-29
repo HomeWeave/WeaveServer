99ca32cfd47e89e0408d405e830237f73893b133
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-06-29 17:09:28 +0530

    Fixing pyling warning.

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,4 +1,12 @@
-from .static_content_handler import root_handler
+"""
+Exposes all the controller blueprints in a list called controllers. When adding
+a new blueprint, add an entry to the controller variable below like:
+    controllers = [
+        ...,
+        (<url_prefix>, blueprint),
+    ]
+"""
+from .static_content_handler import handler as root_handler
 
 controllers = [
     ("/", root_handler),
--- app/controllers/static_content_handler.py
+++ app/controllers/static_content_handler.py
@@ -1,10 +1,14 @@
+"""
+A dummy controller to handle static content.
+"""
 from flask import Blueprint
 
-root_handler = Blueprint('root_handler', __name__, static_folder='static', static_url_path="/static")
+handler = Blueprint('root_handler', __name__, static_folder='static',
+                    static_url_path="/static")
 
-@root_handler.route('')
+@handler.route('')
 def root():
-    with open("static/index.html") as f:
-        return f.read()
+    with open("static/index.html") as inp:
+        return inp.read()
 
 
