039478449e00062bca9f9aa26ceb713a83c6c0aa
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-07-01 21:39:47 +0530

    ShellService: Wait on an event before quitting.

--- app/services/shell_service.py
+++ app/services/shell_service.py
@@ -2,6 +2,8 @@
 Exposes ShellService which has a purpose similar to a shell.
 """
 
+from threading import Event
+
 from app.views.simple_header_view import SimpleHeaderView
 from .base import BaseService, BlockingServiceStart
 #from app.applications import apps as applications
@@ -14,6 +16,10 @@ class ShellService(BaseService, BlockingServiceStart):
 
     def __init__(self, socketio):
         #self.apps = apps or applications
+        self.quit_event = Event()
         view = SimpleHeaderView(self.NAMESPACE, socketio, "Hello!")
         super().__init__(view=view)
+
+    def on_service_start(self, *args, **kwargs):
+        self.quit_event.wait()
 
