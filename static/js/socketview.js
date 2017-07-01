fc21e768fdb28fcc7d3d1cbc58b9c50352cccfb8
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-07-01 21:41:00 +0530

    Extract out socket functionality into a new JS file.

---
+++ static/js/socketview.js
@@ -0,0 +1,39 @@
+function createView(namespace, selector, params) {
+	
+	var targetSelector = selector;
+	var html = null;
+	var args = null;
+
+	var socket = io.connect('http://' + document.domain + ':' + location.port + namespace, {});
+
+	socket.on('connect', function(data) {
+		socket.emit('request_view', {});
+	});
+	
+	socket.on('view', function(data) {
+		html = Handlebars.compile(data.html);
+		args = data.args;
+		updateView();
+	});
+
+	function requestView() {
+		socket.emit("request_view", {});
+	}
+
+	function updateView() {
+		if (targetSelector != null && html != null) {
+			var substHtml = html(args);
+			$(targetSelector).html(substHtml);
+		}
+	}
+
+	return {
+		render: function() {
+			requestView()
+		},
+		reload: function() {
+			requestView();
+		}
+	};
+
+}
