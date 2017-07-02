36a6febb73a9135a0586aa74aa27e5e1a0fbf0fd
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-07-02 17:35:41 +0530

    Bug fixes in the client. Allow for nested views.

--- static/js/socketview.js
+++ static/js/socketview.js
@@ -1,8 +1,10 @@
-function createView(namespace, selector, params) {
+function createView(namespace, target, params) {
 	
-	var targetSelector = selector;
-	var html = null;
+	var rawHtml = null;
+	var compiledHtml = null;
+	var processedHtml = null;
 	var args = null;
+	var innerViews = {};
 
 	var socket = io.connect('http://' + document.domain + ':' + location.port + namespace, {});
 
@@ -11,11 +13,34 @@ function createView(namespace, selector, params) {
 	});
 	
 	socket.on('view', function(data) {
-		if (data.html) {
-			html = Handlebars.compile(data.html);
-		}
 		if (data.args) {
 			args = data.args;
+		}
+		if (data.innerViews) {
+			Object.keys(data.innerViews).forEach(function (name) {
+				var view = data.innerViews[name];
+				var namespace = view.namespace;
+				var randomId = "innerview-" + Math.random().toString(36).substring(2);
+				var tempHtml = '<div id="' + randomId + '"></div>';
+				args[name] = tempHtml;
+				console.log("Will load " + namespace + " into " + randomId);
+
+				var loadInner = function(html) {
+					$("#" + randomId).html(html);
+				}
+
+				var viewObj = createView(namespace, loadInner, {});
+				viewObj.render();
+				innerViews[name] = viewObj;
+
+			});
+		}
+		if (data.html) {
+			rawHtml = data.html;
+			compiledHtml = Handlebars.compile(data.html);
+		}
+		if (compiledHtml) {
+			processedHtml = compiledHtml(args);
 		}
 		updateView();
 	});
@@ -25,9 +50,13 @@ function createView(namespace, selector, params) {
 	}
 
 	function updateView() {
-		if (targetSelector != null && html != null) {
-			var substHtml = html(args);
-			$(targetSelector).html(substHtml);
+		if (processedHtml != null && target != null) {
+			if (typeof target === 'string') {
+				$(target).html(processedHtml);
+			}
+			else if (typeof target === 'function') {
+				target(processedHtml);
+			}
 		}
 	}
 
