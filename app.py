0ce737ef832b56b96c5c2538e889bd73bd583a3b
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-10 23:47:17 -0700

    Use same app structure to launch default UI server, and messaging server.

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import eventlet
 eventlet.monkey_patch()
 
-import socketio
-
-from app.main import create_app
+import sys
 
 
 if __name__ == '__main__':
-    flask_app, sock_app = create_app()
-
-    port = flask_app.config["PORT"]
-
-    sock_app.run(flask_app, host="0.0.0.0", debug=True, use_reloader=False)
+    if sys.argv[1] == 'server':
+        from app.main import create_app
+        flask_app, sock_app = create_app()
+        port = flask_app.config["PORT"]
+        sock_app.run(flask_app, host="0.0.0.0", debug=True, use_reloader=False)
+    elif sys.argv[1] == 'messaging':
+        from app.messaging_app import create_app
+        app = create_app()
+        app.run()
 
