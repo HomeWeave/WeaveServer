8098e5808fbf3340eb96eb64408253c39e35dda1
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-11 23:45:51 -0700

    App management: initial commit.

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import eventlet
 eventlet.monkey_patch()
 
-import sys
-
 
 if __name__ == '__main__':
+    import sys
     if sys.argv[1] == 'server':
         from app.main import create_app
         flask_app, sock_app = create_app()
         port = flask_app.config["PORT"]
         sock_app.run(flask_app, host="0.0.0.0", debug=True, use_reloader=False)
-    elif sys.argv[1] == 'messaging':
-        from app.messaging_app import create_app
-        app = create_app()
-        app.run()
-
+    elif sys.argv[1] == 'launch-app':
+        from app.core.appmgmt import handle_launch
+        handle_launch(sys.argv[1])
