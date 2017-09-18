e29039cf5b1cd0b40b8227ef73c2d5327450c162
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-18 15:29:06 -0700

    Use toposort for sequencing services.

--- app/core/servicemanager.py
+++ app/core/servicemanager.py
@@ -1,12 +1,35 @@
 """
-Contains components that manage services, their sequences and interdependence (later)
+Contains components that manage services, their sequences and interdependence.
 """
 
-import threading
+import importlib
 import logging
+from collections import namedtuple
 
+from app.core.toposort import toposort
 
 logger = logging.getLogger(__name__)
+Module = namedtuple('Module', ["name", "deps", "meta"])
+
+
+def list_modules(module):
+    res = []
+    # for name in os.listdir(module.__path__):
+    for name in ["messaging"]:
+        module = importlib.import_module("app.services." + name)
+        module_meta = module.__meta__
+        deps = module_meta["deps"]
+        res.append(Module(name=name, deps=deps, meta=module_meta))
+    return res
+
+
+def topo_sort_modules(modules):
+    module_map = {x.name: x for x in modules}
+    dep_map = {x.name: x.deps for x in modules}
+    res = []
+    for item in toposort(dep_map):
+        res.append(module_map[item])
+    return res
 
 
 class ServiceManager(threading.Thread):
