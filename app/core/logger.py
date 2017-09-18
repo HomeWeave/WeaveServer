1740f07ffe293498ed5865f39a92121eacdb002a
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-18 15:13:15 -0700

    Use JsonConfig directly for logger.

--- app/core/logger.py
+++ app/core/logger.py
@@ -4,10 +4,10 @@ This module helps configure logging app-wide.
 
 import logging.config
 
-from app.core.config_loader import get_config
+from app.core.config_loader import JsonConfig
 
 
 def configure_logging():
     """ Reads app/configs/logging_config.json to initialize logging config."""
-    config = get_config("logging", {"config.json": "simple"})
-    logging.config.dictConfig(config)
+    config = JsonConfig({ "path": "logging_config.json" })
+    logging.config.dictConfig(config.get())
