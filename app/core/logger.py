366826f6740287dbed5ed461e613d17e9c77d197
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-18 15:33:34 -0700

    Code cleanup.

--- app/core/logger.py
+++ app/core/logger.py
@@ -9,5 +9,5 @@ from app.core.config_loader import JsonConfig
 
 def configure_logging():
     """ Reads app/configs/logging_config.json to initialize logging config."""
-    config = JsonConfig({ "path": "logging_config.json" })
+    config = JsonConfig({"path": "logging_config.json"})
     logging.config.dictConfig(config.get())
