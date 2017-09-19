5d93c11212a10bc77fc15451408f19dfbf75d148
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-18 23:56:42 -0700

    Initial commit TV remote.

---
+++ app/services/tv_remote/__init__.py
@@ -0,0 +1,8 @@
+from .service import TVRemoteService
+
+__meta__ = {
+    "name": "TV Remote Client",
+    "class": TVRemoteService,
+    "deps": ["messaging"],
+    "config": []
+}
---
+++ app/services/tv_remote/service.py
@@ -0,0 +1,28 @@
+import json
+import logging
+
+from app.core.messaging import Receiver
+from app.core.service_base import BaseService, BackgroundProcessServiceStart
+
+
+logger = logging.getLogger(__name__)
+
+
+class TVRemoteReceiver(Receiver):
+    def on_message(self, msg):
+        logger.info("Got msg: %s", json.dumps(msg))
+
+
+class TVRemoteService(BackgroundProcessServiceStart, BaseService):
+    def __init__(self, config):
+        self.receiver = TVRemoteReceiver("/tv/command")
+        super().__init__()
+
+    def get_component_name(self):
+        return "tv_remote"
+
+    def on_service_start(self, *args, **kwargs):
+        self.receiver.run()
+
+    def on_service_stop(self):
+        self.receiver.stop()
