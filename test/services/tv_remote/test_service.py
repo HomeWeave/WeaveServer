006dcc06157d8e1845f92f531a7240598c7529d6
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-29 16:34:49 -0700

    Temporary commit: Add test case.

---
+++ test/services/tv_remote/test_service.py
@@ -0,0 +1,34 @@
+from roku import Roku
+
+from app.core.messaging import Receiver
+from app.core.servicemanager import ServiceManager
+from app.services.tv_remote.service import RokuScanner, RokuTV
+
+
+class TestRokuScanner(object):
+    @classmethod
+    def setup_class(cls):
+        cls.service_manager = ServiceManager(None)
+        cls.service_manager.start_services(["messaging"])
+
+    @classmethod
+    def teardown_class(cls):
+        cls.service_manager.stop()
+
+    def test_basic_discovery(self):
+        roku1 = Roku("abc")
+        scanner = RokuScanner("/devices", scan_interval=10)
+        scanner.discover_devices = lambda: [roku1]
+        scanner.get_device_id = lambda: "deviceid"
+        scanner.start()
+
+        receiver = Receiver("/devices")
+        msg = receiver.receive()
+        expected = {
+            "deviceid": {
+                "device_id": "deviceid",
+                "device_command_queue": "devices/tv/command",
+                "device_commands": RokuTV(None, None).read_commands()
+            }
+        }
+        assert msg == expected
