88a04efd1a7aa56a69b76b127908a5eca0c817bd
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-29 17:27:47 -0700

    Use Fake redis for tests.

--- test/services/tv_remote/test_service.py
+++ test/services/tv_remote/test_service.py
@@ -1,3 +1,5 @@
+import os
+
 from roku import Roku
 
 from app.core.messaging import Receiver
@@ -8,11 +10,13 @@ from app.services.tv_remote.service import RokuScanner, RokuTV
 class TestRokuScanner(object):
     @classmethod
     def setup_class(cls):
+        os.environ["USE_FAKE_REDIS"] = "TRUE"
         cls.service_manager = ServiceManager(None)
         cls.service_manager.start_services(["messaging"])
 
     @classmethod
     def teardown_class(cls):
+        del os.environ["USE_FAKE_REDIS"]
         cls.service_manager.stop()
 
     def test_basic_discovery(self):
