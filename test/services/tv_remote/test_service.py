718ed678439356a57104dfcf74e63ce89a3cfcea
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-09-29 23:02:26 -0700

    Add tests. Run/stop message server per test case.

--- test/services/tv_remote/test_service.py
+++ test/services/tv_remote/test_service.py
@@ -8,16 +8,14 @@ from app.services.tv_remote.service import RokuScanner, RokuTV
 
 
 class TestRokuScanner(object):
-    @classmethod
-    def setup_class(cls):
+    def setup_method(self):
         os.environ["USE_FAKE_REDIS"] = "TRUE"
-        cls.service_manager = ServiceManager(None)
-        cls.service_manager.start_services(["messaging"])
+        self.service_manager = ServiceManager(None)
+        self.service_manager.start_services(["messaging"])
 
-    @classmethod
-    def teardown_class(cls):
+    def teardown_method(self):
         del os.environ["USE_FAKE_REDIS"]
-        cls.service_manager.stop()
+        self.service_manager.stop()
 
     def test_basic_discovery(self):
         roku1 = Roku("abc")
@@ -36,6 +34,38 @@ class TestRokuScanner(object):
                 "device_commands": RokuTV(None, None).read_commands()
             }
         }
+        assert msg == expected
+
+        receiver.stop()
+        scanner.stop()
+
+    def test_new_tv_discovery(self):
+        roku1 = Roku("abc")
+        roku2 = Roku("def")
+        scanner = RokuScanner("/devices")
+        scanner.discover_devices = lambda: [roku1]
+        scanner.get_device_id = lambda x: x
+        scanner.start()
+
+        receiver = Receiver("/devices")
+        receiver.start()
+        msg = receiver.receive().task.data
+        expected = {
+            "abc": {
+                "device_id": "abc",
+                "device_commands_queue": "/device/tv/command",
+                "device_commands": RokuTV(None, None).read_commands()
+            }
+        }
+        assert msg == expected
+
+        scanner.discover_devices = lambda: [roku1, roku2]
+        scanner.scan()
+
+        msg = receiver.receive().task.data
+        expected["def"] = expected["abc"].copy()
+        expected["def"]["device_id"] = "def"
+
         assert msg == expected
 
         receiver.stop()
