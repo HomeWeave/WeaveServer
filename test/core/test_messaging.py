f042739fa8f4c2c4c6495b826354c01ea01267da
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-10-03 23:36:07 -0700

    Bug fix in unit test cases.

--- test/core/test_messaging.py
+++ test/core/test_messaging.py
@@ -1,7 +1,5 @@
 from threading import Event, Thread
 
-import pytest
-
 import app.core.netutils as netutils
 from app.core.messaging import discover_message_server
 from app.services.discovery import DiscoveryService
@@ -11,34 +9,27 @@ class TestDiscoverMessageServer(object):
     def test_no_discovery_server(self):
         assert discover_message_server() is None
 
-    @pytest.mark.skip(reason="Fails")
     def test_discovery_server_bad_json(self):
         service = DiscoveryService(None)
-        service.process = lambda x, y: "sdf;lghkd;flasgkh"
+        service.server.process = lambda x, y: "sdf;lghkd;flkh".encode("UTF-8")
         event = Event()
         service.notify_start = event.set
         Thread(target=service.on_service_start).start()
         event.wait()
 
-        try:
-            assert discover_message_server() is None
-        finally:
-            service.on_service_stop()
+        assert discover_message_server() is None
+        service.on_service_stop()
 
-    @pytest.mark.skip(reason="Fails")
     def test_discovery_server_unknown_json(self):
         service = DiscoveryService(None)
-        service.process = lambda x, y: '{"valid": "json"}'
+        service.server.process = lambda x, y: '{"vld": "json"}'.encode("UTF-8")
         event = Event()
         service.notify_start = event.set
         Thread(target=service.on_service_start).start()
         event.wait()
 
-        try:
-            assert discover_message_server() is None
-        finally:
-            service.on_service_stop()
-
+        assert discover_message_server() is None
+        service.on_service_stop()
 
     def test_valid_discovery_server(self):
         service = DiscoveryService(None)
