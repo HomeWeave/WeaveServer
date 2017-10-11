e0b6834b6ed0f5e0d49b6f9eb19847aced68f645
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-10-10 22:09:33 -0700

    Hopefully this fixes travis.

--- test/core/test_netutils.py
+++ test/core/test_netutils.py
@@ -3,7 +3,7 @@ from app.core.netutils import ping_host
 
 class TestPingHost(object):
     def test_good_host(self):
-        assert ping_host("localhost")
+        assert ping_host("127.0.0.1")
 
     def test_bad_host(self):
         assert not ping_host("sdlvhsdlkhdfg")
