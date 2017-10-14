66a27110a81e71f78b607453330ff1b9194f96e5
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-10-13 23:54:20 -0700

    Bug fixes and code clean up.

--- app/services/updater/service.py
+++ app/services/updater/service.py
@@ -1,11 +1,11 @@
 import logging
 import os
 import subprocess
-import time
-from threading import Timer, Thread, Event
+from threading import Thread, Event
 
 from git import Repo
 from git.util import RemoteProgress
+from git.exc import GitError
 from retask import Task
 
 from app.core.messaging import Receiver, Sender
@@ -14,7 +14,7 @@ from app.core.service_base import BaseService, BackgroundProcessServiceStart
 
 logger = logging.getLogger(__name__)
 SCRIPTS_DIR = "/home/rpi/scripts"
-CODE_DIR = "/home/rpi/Code"
+CODE_DIR = os.path.expanduser("~/Code")
 
 
 class PercentProgressIndicator(RemoteProgress):
@@ -79,6 +79,7 @@ class UpdateScanner(object):
         self.repos_to_update = []
 
     def start(self):
+        self.notification_sender.start()
         self.cancel_event.clear()
         self.thread.start()
 
@@ -93,25 +94,35 @@ class UpdateScanner(object):
 
     def check_updates(self):
         res = []
-        all_repos = self.get_repos()
+        all_repos = self.list_repos(CODE_DIR)
 
         if not all_repos:
             return []
 
         logger.info("Checking for updates: %s", str(all_repos))
         for count, path in enumerate(all_repos):
-            repo = Repository(path)
+            try:
+                repo = self.get_repo(path)
 
-            if repo.needs_pull():
-                res.append(repo)
-        self.repos_to_update = res
+                if repo.needs_pull():
+                    res.append(repo)
+            except GitError as e:
+                logger.warning("Unable to fetch %s", path, e)
+
+        if res:
+            self.repos_to_update = res
+            self.notify_updates()
         return res
 
     def notify_updates(self):
+        logger.info("Updates found.")
         self.notification_sender.send(Task({"message": "Update available."}))
 
-    def get_repos(self, path):
+    def list_repos(self, path):
         return [os.path.join(path, x) for x in os.listdir(path)]
+
+    def get_repo(self, path):
+        return Repository(path)
 
     @property
     def updates(self):
@@ -137,9 +148,9 @@ class Updater(Receiver):
         for repo in repos:
             repo.pull(self.send_pull_progress)
 
-        self.perform_update()
+        self.perform_ansible_update()
 
-    def perform_update(self):
+    def perform_ansible_update(self):
         logger.info("Running ansible")
         run_ansible_path = os.path.join(SCRIPTS_DIR, "run_ansible.sh")
         args = [run_ansible_path]
@@ -156,6 +167,7 @@ class UpdaterService(BackgroundProcessServiceStart, BaseService):
     def __init__(self, config):
         self.update_scanner = UpdateScanner("/services/shell/notifications")
         self.updater = Updater(self.update_scanner, "/services/updater/command")
+        self.shutdown = Event()
         super().__init__()
 
     def get_component_name(self):
@@ -165,7 +177,9 @@ class UpdaterService(BackgroundProcessServiceStart, BaseService):
         self.update_scanner.start()
         self.updater.start()
         self.notify_start()
+        self.shutdown.wait()
 
     def on_service_stop(self):
         self.updater.stop()
         self.update_scanner.stop()
+        self.shutdown.set()
