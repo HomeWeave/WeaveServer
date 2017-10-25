de66e36c34605f82f4b89aecb281b245121eaa30
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-10-24 20:15:31 -0700

    create queue is not a sticky queue.

--- app/services/messaging/system_queues.py
+++ app/services/messaging/system_queues.py
@@ -1,7 +1,6 @@
 SYSTEM_QUEUES = [
     {
         "queue_name": "_system/app-queues/create",
-        "queue_type": "keyedsticky",
         "request_schema": {
             "type": "string"
         }
