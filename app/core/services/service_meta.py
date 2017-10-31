9f5a1104d9e2da8306841ed71abebf672dc7136d
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-10-30 20:36:57 -0700

    Initial commit: Event queues.

--- app/core/services/service_meta.py
+++ app/core/services/service_meta.py
@@ -1,27 +1,109 @@
+from copy import deepcopy
+from threading import Thread
 from uuid import uuid4
 
-from app.core.messaging import Message
+from jsonschema import validate, ValidationError
+
+from app.core.messaging import Message, Creator, Receiver
 
 
-class Event(Message):
-    def __init__(self, name):
-        self.uuid = str(uuid4())
-        obj = {
-            "name": name,
-            "id": self.uuid,
+def create_capability_queue(queue_name):
+    queue_info = {
+        "queue_name":queue_name,
+        "queue_type": "keyedsticky",
+        "request_schema": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "description": {"type": "string"},
+                "params": {"type": "object"},
+                "id": {"type": "string"},
+            }
         }
-        super().__init__("enqueue", obj)
-
-    def unique_id(self):
-        return self.uuid
+    }
+    creator = Creator()
+    creator.start()
+    creator.create(queue_info)
 
 
 class Capability(Message):
-    def __init__(self, name, description):
+    def __init__(self, name, description, params):
         self.uuid = str(uuid4())
+        self.params = deepcopy(params)
         obj = {
             "name": name,
             "description": description,
+            "params": self.params,
             "id": self.uuid,
         }
         super().__init__("enqueue", obj)
+
+    @property
+    def unique_id(self):
+        return self.uuid
+
+    @property
+    def parameters(self):
+        return self.params
+
+
+class EventReceiver(Receiver):
+    def __init__(self, queue_name, capability, handler):
+        super().__init__(queue_name)
+        self.capability = capability
+        self.handler = handler
+        self.schema = {
+            "type": "object",
+            "properties": capability.parameters,
+            "required": list(capability.keys())
+        }
+
+    def on_message(self, msg):
+        try:
+            validate(msg, self.schema)
+        except ValidationError:
+            logger.warning("Failed EventReceiver schema validation: %s", msg)
+            return
+
+        self.handler(**msg)
+
+
+class EventDrivenService(object):
+    def express_capability(self, capability, handler):
+        if not hasattr(self, 'event_driven_intialized'):
+            self.initialize_event_driven_service()
+
+        queue_name = self.get_service_queue_name("capabilities")
+        capability_sender = Sender(queue_name)
+        headers = {"KEY": capability.unique_id}
+        capability_sender.send(capability, headers=headers)
+
+        self.create_event_queue(capability)
+        self.start_event_receiver(queue_name, capability, handler)
+
+    def initialize_event_driven_service(self, queue_name):
+        create_capabilities_queue(self.get_service_queue_name("capabilities"))
+
+    def create_event_queue(self, capability):
+        qid = capability.unique_id
+        queue_name = self.get_service_queue_name("capability/" + qid)
+        queue_info = {
+            "queue_name": queue_name,
+            "request_schema": capability.schema
+        }
+        creator = Creator()
+        creator.start()
+        creator.create(queue_info)
+
+    def start_event_receiver(self, queue_name, capability, handler):
+        params = (queue_name, capability, handler)
+        thread = Thread(target=self.run_event_receiver, args=params)
+        thread.start()
+
+    def run_event_receiver(self, queue_name, capability, handler):
+        receiver = EventReceiver(queue_name, capability, handler)
+        receiver.start()
+        receiver.run()
+
+    def get_service_queue_name(self, queue_name):
+        return "/services/{}/{}".format(service_name, queue_name)
