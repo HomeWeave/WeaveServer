29c923a3052d10da19c48ccd3a61a22a9d9134c7
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-11-06 17:26:02 -0800

    Create event and capability queues. Register service upon start.

--- app/core/services/service_meta.py
+++ app/core/services/service_meta.py
@@ -30,6 +30,24 @@ def create_capabilities_queue(queue_name):
     creator.start()
     creator.create(queue_info)
 
+def create_capability_queue(queue_name):
+    queue_info = {
+        "queue_name": queue_name,
+        "queue_type": "keyedsticky",
+        "request_schema": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "description": {"type": "string"},
+                "params": {"type": "object"},
+                "id": {"type": "string"}
+            }
+        }
+    }
+    creator = Creator()
+    creator.start()
+    creator.create(queue_info)
+
 def check_handler_param(params, handler):
     schema_args = set(params.keys())
     handler_args = set(signature(handler).parameters.keys())
@@ -39,22 +57,18 @@ def check_handler_param(params, handler):
 
 class Capability(Message):
     def __init__(self, name, description, params, queue_template):
-        self.uuid = str(uuid4())
+        self.unique_id = str(uuid4())
         self.params = deepcopy(params)
         self.name = name
-        self.queue = queue_template.format(self.uuid)
+        self.queue = queue_template.format(self.unique_id)
         obj = {
             "name": name,
             "description": description,
             "params": self.params,
-            "id": self.uuid,
+            "id": self.unique_id,
             "queue": self.queue
         }
         super().__init__("enqueue", obj)
-
-    @property
-    def unique_id(self):
-        return self.uuid
 
     @property
     def schema(self):
@@ -63,6 +77,33 @@ class Capability(Message):
             "properties": self.params,
             "required": list(self.params.keys())
         }
+
+
+class Event(object):
+    def __init__(self, name, description, params, queue_template):
+        self.unique_id = str(uuid4())
+        self.name = name
+        self.description = description
+        self.params = params
+        self.queue = queue_template.format(self.unique_id)
+        self.sender = Sender(self.queue)
+
+    def build_announcement(self):
+        data = {
+            "name": self.name,
+            "description": self.description,
+            "params": self.params,
+            "id": self.unique_id,
+            "queue": self.queue
+        }
+        return Message("enqueue", data)
+
+    @property
+    def schema(self):
+        return {"type": "string"}
+
+    def fire(self):
+        self.sender.send(self.unique_id)
 
 
 class EventReceiver(Receiver):
@@ -82,8 +123,10 @@ class EventReceiver(Receiver):
 class EventDrivenService(object):
     def on_service_start(self, *args, **kwargs):
         create_capabilities_queue(self.get_service_queue_name("capabilities"))
+        create_events_queue(self.get_service_queue_name("events"))
         self.capabilities_queue_map = {}
         self.capabilities_queue_lock = RLock()
+        self.register_service()
         super().on_service_start(*args, **kwargs)
 
     def express_capability(self, name, description, params, handler):
@@ -96,13 +139,24 @@ class EventDrivenService(object):
         headers = {"KEY": capability.unique_id}
         capability_sender.send(capability, headers=headers)
 
-        self.create_event_queue(capability)
-        thread, receiver = self.start_event_receiver(capability, handler)
+        self.create_capability_queue(capability)
+        thread, receiver = self.start_capability_receiver(capability, handler)
         with self.capabilities_queue_lock:
             self.capabilities_queue_map[capability.unique_id] = (thread,
                                                                  receiver)
 
-    def create_event_queue(self, capability):
+    def express_event(self, name, description, params):
+        queue_template = self.get_service_queue_name("event/{}")
+        event = Event(name, description, params)
+        queue_name = self.get_service_queue_name("events")
+        event_sender = Sender(queue_name)
+        event_sender.start()
+        headers = {"KEY": event.unique_id}
+        event_sender.send(event.build_announcement(), headers=headers)
+
+        self.create_event_queue(event)
+
+    def create_capability_queue(self, capability):
         qid = capability.unique_id
         queue_name = self.get_service_queue_name("capability/" + qid)
         queue_info = {
@@ -113,7 +167,7 @@ class EventDrivenService(object):
         creator.start()
         creator.create(queue_info)
 
-    def start_event_receiver(self, capability, handler):
+    def start_capability_receiver(self, capability, handler):
         qid = capability.unique_id
         queue_name = capability.queue
         receiver = EventReceiver(queue_name, capability, handler)
@@ -122,8 +176,23 @@ class EventDrivenService(object):
         thread.start()
         return thread, receiver
 
-    def run_event_receiver(self, receiver):
-        receiver.run()
+    def create_event_queue(self, event):
+        qid = event.unique_id
+        queue_name = self.get_service_queue_name("event/" + qid)
+        queue_info = {
+            "queue_name": queue_name,
+            "request_schema": {"type": "object"}
+        }
+
+        creator = Creator()
+        creator.start()
+        creator.create(queue_info)
+
+    def register_service(self):
+        sender = Sender("/root/services")
+        sender.start()
+        headers = {"KEY": self.get_service_queue_name("")}
+        sender.send({}, headers=headers)
 
     def get_service_queue_name(self, queue_name):
         service_name = self.get_component_name()
