1b03509015442c51c09940f7b9bfe08ad712e228
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-12-24 22:34:21 +0530

    Static content.

--- app/services/dashboard/static/js/main.js
+++ app/services/dashboard/static/js/main.js
@@ -1,59 +1,4 @@
-var Library = function(caja, appInfo) {
-    var libraries = {
-        "basic": {
-            "template": function(template) {
-                var compiled = Handlebars.compile(template);
-                caja.markFunction(compiled);
-                return compiled;
-            },
-            "Socket": function(params) {
-                var sock = ViewSocket(appInfo.namespace, params);
-                caja.markFunction(sock.send);
-                return sock;
-            },
-            "alert": function() {
-                alert("[" + appInfo.name + "]" + arguments[0])
-            },
-            "log": function() {
-                 console.log.apply(console, arguments);
-            }
-        }
-    };
-    var allowed = ["basic"];
-    var allFuncs = allowed.map(function(x) {
-        return libraries[x];
-    }).reduce(function(state, item) {
-        Object.assign(state, item);
-        return state;
-    }, {});
-    Object.keys(allFuncs).forEach(function(key) {
-        var func = allFuncs[key];
-        allFuncs[key] = caja.tame(caja.markFunction(func));
-    });
-    return allFuncs;
-}
-
-
-var ViewAnimator = {
-    default: function(selector, html) {
-        $(selector).html(html);
-    },
-    fadeIn: function(selector, html) {
-        var obj = $(selector);
-        obj.hide();
-        obj.html(html);
-        obj.fadeIn();
-    },
-    riseUp: function(selector, html) {
-        var obj = $(selector);
-        obj.css('opacity', 0);
-        obj.html(html);
-        obj.slideDown('slow').animate({ opacity: 1 }, { queue: false, duration: 'slow' });
-    }
-}
-
-
-var ViewSocket = function(namespace, params) {
+var Socket = function(namespace, params) {
     params.listeners = params.listeners || {};
     params.initMsg = params.initMsg || 'first-dummy-message';
 
@@ -67,7 +12,7 @@ var ViewSocket = function(namespace, params) {
     socket.on('connect', function(data) {
         socket.emit(params.initMsg, {});
     });
-    
+
     var defaultListeners = {
         "socket_disconnect": function(data) {
             socket.disconnect();
@@ -94,121 +39,43 @@ var ViewSocket = function(namespace, params) {
     }
 }
 
-var ViewManager = function(selector) {
-    var sly = new Sly(selector, {
-        horizontal: 1,
-        itemNav: 'forceCentered',
-        smart: 1,
-        activateMiddle: 1,
-        mouseDragging: 1,
-        touchDragging: 1,
-        releaseSwing: 1,
-        startAt: 0,
-        scrollBy: 1,
-        speed: 300,
-        elasticBounds: 1,
-        easing: 'easeOutExpo',
-        dragHandle: 1,
-        dynamicHandle: 1,
-        clickBar: 1,
-    }).init();
-    
-    function findAppElem(appId) {
-        return $(selector + ' li[data-appid~="' + appId + '"]')[0];
+var DockComponent = function(selector) {
+    var template = Handlebars.compile($(selector + " ul").html());
+
+    function showDockItem(selector, html) {
+        var duration = 100 + Math.random() * 500;
+        $(html.trim()).hide().appendTo(selector).fadeIn(duration);
     }
 
-    var appTemplate = Handlebars.compile($("#application-wrap-template").html());
-
-    return {
-        activateApp: function(appId) {
-            var elem = findAppElem(appId);
-            if (elem === undefined) {
-                console.log("No app: " + appId + " to activate.");
-                return false;
+    var socket = Socket("/dock", {
+        initMsg: "list",
+        listeners: {
+            "dock_apps": function(data) {
+                var ul = $(selector + " ul");
+                ul.html("");
+                var html = Object.keys(data).map(function(key) {
+                    var service = data[key];
+                    service.icon = service.icon || "cog";
+                    var html = template(service);
+                    showDockItem(selector + " ul", html);
+                });
             }
-            sly.activate(elem);
-        },
-        listApps: function() {
-            return $(selector + " li[data-appid]").map(function() {
-                return $(this).data("appid");
-            });
-        },
-        addApp: function(appInfo, onLoadFunc) {
-            var elem = findAppElem(appInfo.id);
-            if (elem !== undefined) {
-                sly.remove(elem);
-            }
-
-            var html = appTemplate(appInfo);
-            var nodes = $.parseHTML($.trim(html));
-
-            sly.add(nodes[0])
-
-            var container = nodes[0].querySelector(".application-wrap");
-
-            var cajaObj = Caja(appInfo, container);
-            cajaObj.load(onLoadFunc);
-            console.log("Application loaded:", appInfo.name);
-        },
-        removeApp: function(appId) {
-            var elem = findAppElem(appInfo.id);
-            if (elem === undefined) {
-                return false;
-            }
-            sly.remove(elem);
-            return true;
         }
-    }
-};
+    });
+}
 
-var Caja = function(appInfo, container) {
-    var uriPolicy = {
-        rewrite: function(x) { return x;}
-    };
+var TopBar = function(selector) {
+    setInterval(function() {
+        $(selector).html(new Date().toISOString().replace(/[TZ]/g, " "));
+    }, 1000);
+}
 
-    return {
-        load: function(onLoadFunc) {
-            caja.load(container, caja.policy.net.ALL, function(frame) {
-                frame.code("/", 'text/html', appInfo.html);
-                frame.api(Library(caja, appInfo));
-                frame.run();
-
-                if (onLoadFunc !== undefined) {
-                    onLoadFunc();
-                }
-            });
-        }
-    };
+var Shell = function(selector) {
+    var socket = Socket("/shell", {});
+    var dock = DockComponent("#dock-contents");
+    var topbar = TopBar("#page-topbar");
 }
 
 $(document).ready(function() {
-    
-    caja.initialize({
-        es5Mode: true,
-        cajaServer: 'https://caja.appspot.com/',
-        debug: true
-    });
-
-    viewManager = ViewManager("#oneperframe");
-
-    var socket = ViewSocket("/shell", {
-        listeners: {
-            "active_apps": function(data) {
-                data.apps.forEach(function(app) {
-                    viewManager.addApp(app);
-                });
-                viewManager.activateApp(data.activeAppId);
-            },
-            "launch_app": function(data) {
-                viewManager.addApp(data, function() {
-                    console.log(data);
-                    viewManager.activateApp(data.id);
-                });
-            },
-            "activate_app": function(data) {
-                viewManager.activateApp(data.id);
-            }
-        },
-        initMsg: 'get_active_apps'
-    });
+    var shell = Shell("#page-wrapper");
 });
