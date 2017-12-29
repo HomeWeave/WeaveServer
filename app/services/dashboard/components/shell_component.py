c929569dc755b131eedf8b4ff3de320c8d480dc9
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-12-29 21:43:01 +0530

    Making RPC work.

--- app/services/dashboard/components/shell_component.py
+++ app/services/dashboard/components/shell_component.py
@@ -1,18 +1,69 @@
+import logging
+from threading import Thread, RLock
+
+from app.core.messaging import Receiver, Sender
+
 from .base import BaseComponent
-from .dock_component import DockComponent
+
+logger = logging.getLogger(__name__)
+
+
+class AppLister(object):
+    def __init__(self, shell_component):
+        self.apps = []
+        self.shell_component = shell_component
+        self.rpc_list_receiver = Receiver("/_system/rpc-servers")
+
+    def start(self):
+        self.rpc_list_receiver.on_message = self.process_rpc_list_message
+        self.rpc_list_receiver.start()
+
+        self.receiver_thread = Thread(target=self.rpc_list_receiver.run)
+        self.receiver_thread.start()
+
+    def stop(self):
+        self.rpc_list_receiver.stop()
+        self.receiver_thread.join()
+
+    def process_rpc_list_message(self, msg):
+        self.apps = msg
+        for obj in self.apps.values():
+            obj["kind"] = "RPC"
+
+        self.shell_component.notify_all('dock_apps', self.apps)
 
 
 class ShellComponent(BaseComponent):
     def __init__(self, ws_manager):
         super(ShellComponent, self).__init__("/shell")
         self.ws_manager = ws_manager
-        self.apps_stack = []
-        self.dock_component = DockComponent(self)
+        self.app_lister = AppLister(self)
+        self.senders = {}
+        self.senders_lock = RLock()
 
     def activate(self):
-        self.ws_manager.register(self.dock_component)
-        self.dock_component.activate()
+        self.app_lister.start()
 
     def deactivate(self):
-        self.dock_component.deactivate()
-        self.ws_manager.unregister(self.dock_component)
+        self.app_lister.stop()
+
+    def on_list_apps(self, msg):
+        self.reply('dock_apps', self.app_lister.apps)
+
+    def on_messaging(self, obj):
+        uri = obj["uri"]
+        data = obj["data"]
+        # self.get_sender(uri).send(data)
+        sender = Sender(uri)
+        sender.start()
+        logger.info("Sent Message to %s: %s", uri, str(data))
+        sender.send(data)
+
+    def get_sender(self, queue):
+        with self.senders_lock:
+            if queue in self.senders:
+                return self.senders[queue]
+            sender = Sender(queue)
+            self.senders[queue] = sender
+        sender.start()
+        return sender
--- app/services/dashboard/static/js/main.js
+++ app/services/dashboard/static/js/main.js
@@ -1,3 +1,7 @@
+Handlebars.registerHelper('equals', function(op1, op2, options) {
+    return op1 == op2? options.fn(this): options.inverse(this);
+});
+
 var Socket = function(namespace, params) {
     params.listeners = params.listeners || {};
     params.initMsg = params.initMsg || 'first-dummy-message';
@@ -35,33 +39,117 @@ var Socket = function(namespace, params) {
         },
         close: function() {
             socket.disconnect();
+        },
+        register: function(key, fn) {
+            if (typeof fn !== 'function') {
+                return false;
+            }
+            socket.on(key, fn)
+        },
+        unregister: function(key) {
+            socket.off(key);
         }
     }
 }
 
-var DockComponent = function(selector) {
-    var template = Handlebars.compile($(selector + " ul").html());
-
-    function showDockItem(selector, html) {
-        var duration = 100 + Math.random() * 500;
-        $(html.trim()).hide().appendTo(selector).fadeIn(duration);
-    }
-
-    var socket = Socket("/dock", {
-        initMsg: "list",
-        listeners: {
-            "dock_apps": function(data) {
-                var ul = $(selector + " ul");
-                ul.html("");
-                var html = Object.keys(data).map(function(key) {
-                    var service = data[key];
-                    service.icon = service.icon || "cog";
-                    var html = template(service);
-                    showDockItem(selector + " ul", html);
-                });
-            }
+var MessagingChannel = function(socket, queueName) {
+    var receivers = {};
+    socket.register("messaging", function(obj) {
+        if (receivers[obj.queue] !== undefined) {
+            receivers[obj.queue](obj.data);
         }
     });
+
+    return {
+        register: function(queue, handler) {
+            receivers[queue] = handler;
+        },
+        unregister: function(queue) {
+            delete receivers[queue];
+        },
+        send: function(queue, obj) {
+            socket.send("messaging", {queue: queue, data: obj});
+        }
+    }
+};
+
+var ApplicationRPC = function() {
+    var rpcTemplate = Handlebars.compile($("#rpc-template").html());
+
+    function buildRPCContent(socket, appInfo, $node) {
+        var html = rpcTemplate(appInfo);
+        $node.append(html).on('click', 'form [type=submit]', function() {
+            var $form = $(this).closest('form');
+            var api = appInfo.apis[$form.data("api-id")];
+            var args = (api.args || []).map(function(arg) {
+                if (arg.type == "toggle") {
+                    return $("[name=" + arg.name + "]").is(":checked");
+                } else if (arg.type == "number") {
+                    return parseInt($("[name=" + arg.name + "]").val());
+                }
+                return $("[name=" + arg.name + "]").val();
+            });
+            var kwargsParams = Object.keys(api.kwargs || {});
+            var kwargs = kwargsParams.reduce(function(state, cur) {
+                if (api.kwargs[cur].type == "toggle") {
+                    state[cur] = $("[name=" + cur + "]").is(":checked");
+                } else if (api.kwargs[cur].type == "number") {
+                    state[cur] = parseInt($("[name=" + cur + "]").val());
+                } else {
+                    state[cur] = $("[name=" + cur + "]").val();
+                }
+                return state;
+            }, {});
+
+            var data = {
+                command: api.id,
+                args: args,
+                kwargs: kwargs
+            };
+            socket.send("messaging", {data: data, uri: $form.data("uri")});
+            console.log("Sent to uri: ", $form.data("uri"), ", data: ", data);
+            return false;
+        });
+    };
+
+    return {
+        build: buildRPCContent,
+    }
+}
+
+var DockComponent = function(selector, appManager) {
+    var template = Handlebars.compile($(selector).html());
+
+    function addDockItem(selector, service) {
+        var html = template(service);
+        var duration = 100 + Math.random() * 1000;
+        var dom = $(html.trim());
+
+        dom.click(function() {
+            appManager.launch(service);
+        });
+
+        dom.appendTo(selector);
+        dom.hide().fadeIn(duration);
+    }
+
+    var curItems = {};
+
+    return {
+        addItems: function(data) {
+            $(selector).html("");
+            var newItems = Object.keys(data).filter(function(key) {
+                return !(key in curItems);
+            });
+            var html = newItems.map(function(key) {
+                var service = data[key];
+                service.icon = service.icon || "cog";
+                addDockItem(selector, service);
+                curItems[key] = service;
+            });
+        }
+    }
+
 }
 
 var TopBar = function(selector) {
@@ -70,10 +158,75 @@ var TopBar = function(selector) {
     }, 1000);
 }
 
+var ApplicationManager = function(selector, socket) {
+    var sly = new Sly(selector, {
+        horizontal: 1,
+        itemNav: 'forceCentered',
+        smart: 1,
+        activateMiddle: 1,
+        mouseDragging: 1,
+        touchDragging: 1,
+        releaseSwing: 1,
+        startAt: 0,
+        scrollBy: 1,
+        speed: 300,
+        elasticBounds: 1,
+        easing: 'easeOutExpo',
+        dragHandle: 1,
+        dynamicHandle: 1,
+        clickBar: 1,
+    }).init();
+
+    var rpc_builder = ApplicationRPC();
+
+    function findAppElem(appId) {
+        return $(selector + ' [data-appid~="' + appId + '"]')[0];
+    }
+
+
+    function buildApplicationContent(appInfo, node) {
+        if (appInfo.kind == "RPC") {
+            var contentNode = $(node).find(".application-content");
+            rpc_builder.build(socket, appInfo, contentNode);
+        }
+    }
+
+    var appTemplate = Handlebars.compile($(selector + " ul").html());
+    $(selector + " ul").html("");
+
+    return {
+        "launch": function(app) {
+            var appElem = findAppElem(app.id);
+            if (appElem !== undefined) {
+                sly.activate(appElem);
+                return;
+            }
+
+            var html = appTemplate(app);
+            var nodes = $.parseHTML($.trim(html));
+            buildApplicationContent(app, nodes[0]);
+
+            sly.add(nodes[0]);
+            sly.activate(nodes[0]);
+        },
+        "close": function(app) {
+            var appElem = findAppElem(app.id);
+            if (appElem !== undefined) {
+                sly.remove(appElem);
+            }
+        }
+    };
+}
+
 var Shell = function(selector) {
     var socket = Socket("/shell", {});
-    var dock = DockComponent("#dock-contents");
+
+    var appManager = ApplicationManager("#oneperframe", socket);
+    var dock = DockComponent("#dock-contents", appManager);
     var topbar = TopBar("#page-topbar");
+
+    socket.register("dock_apps", dock.addItems);
+    socket.send("list_apps", {});
 }
 
 $(document).ready(function() {
