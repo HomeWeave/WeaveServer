d1683dd4fb8779890422a64b3ac8944b89590bd9
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-01-28 00:32:14 +0530

    Use only setuptools with find_packages.

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='weaveserver',
     version='0.8',
     author='Srivatsan Iyer',
     author_email='supersaiyanmode.rox@gmail.com',
-    packages=['weaveserver'],
+    packages=find_packages(),
     license='MIT',
     description='Library to interact with Weave Server',
     long_description=open('README.md').read(),
