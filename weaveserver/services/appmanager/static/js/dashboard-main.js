35990b9e47c3400c7aed65fe1b2646a564ecfd7a
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-05-28 16:21:28 -0700

    Temporary commit static content.

--- weaveserver/services/appmanager/static/js/dashboard-main.js
+++ weaveserver/services/appmanager/static/js/dashboard-main.js
@@ -1,40 +1,56 @@
+function loadSystemCards(data) {
+    var smallCards = [
+        {
+            cardType: 'success',
+            icon: {material: 'build'},
+            title: 'Version',
+            content: data.version,
+            footer: [
+                {
+                    icon: {material: 'build'},
+                    url: "#",
+                    message: "Test"
+                }
+            ]
+        },
+        {
+            cardType: 'warning',
+            icon: {material: 'dns'},
+            title: 'Rules',
+            content: data.rules + " active",
+            footer: []
+        },
+        {
+            cardType: 'info',
+            icon: {material: 'developer_board'},
+            title: 'Plugins',
+            content: data.plugins + " loaded",
+            footer: []
+        },
+    ];
+    var cards = smallCards.forEach(function(data) {
+        var card = SmallCard('.content .weave-small-cards-row');
+        card.load(data);
+        return card;
+    });
+}
+
+function loadComponents(components) {
+    components.forEach(function(component) {
+        var card = MediumCard('.content .weave-medium-cards-row');
+        card.load(component);
+    });
+}
+
 $(document).ready(function() {
+    registerComponents();
     var statusRequest = $.ajax({
         url: "/api/status",
         dataType: "json"
     });
 
     $.when(statusRequest).then(function(statusResponse) {
-        var smallCards = [
-            {
-                cardType: 'success',
-                icon: {material: 'build'},
-                title: 'Version',
-                content: statusResponse.version,
-                footer: []
-            },
-            {
-                cardType: 'warning',
-                icon: {material: 'dns'},
-                title: 'Rules',
-                content: statusResponse.rules + " rules active",
-                footer: []
-            },
-            {
-                cardType: 'info',
-                icon: {material: 'developer_board'},
-                title: 'Plugins',
-                content: statusResponse.plugins + " loaded",
-                footer: []
-            },
-        ];
-
-        var mediumCards = statusResponse.components;
-
-        var smallCard = SmallCard();
-        var mediumCard = MediumCard('.content .weave-medium-cards-row');
-
-        $('.content .weave-small-cards-row').html($.map(smallCards, smallCard.html).join(""));
-        mediumCards.forEach(mediumCard.load);
+        loadSystemCards(statusResponse);
+        loadComponents(statusResponse.components);
     });
 });
--- weaveserver/services/appmanager/static/js/dashcomponents.js
+++ weaveserver/services/appmanager/static/js/dashcomponents.js
@@ -1,59 +1,105 @@
-function Context() {
-	var variables = {};
-
-	return {
-		get: function(val) { return variables[val]; },
-		set: function(key, val) {
-			variables[key] = val;
-		}
-	}
+function registerComponents() {
+    Vue.component('card-footer-status', {
+      template: '#template-footer-status',
+      props: ['footer']
+    });
 }
 
-function ActionEvaluator(context) {
-	function rpc(data) {
-		
-	}
+function Actions(app, actions) {
+    function rpc(action, previousResult) {
+        return $.ajax({
+            url: "/api/rpc",
+            type: 'post',
+            contentType: 'application/json; charset=UTF-8',
+            data: JSON.stringify(action.data)
+        });
+    }
 
-	var handlers = {rpc: rpc};
-	return {
-		evaluate: function(action) {
-			var handler = handlers[action.type];
-			if (handler === undefined) { return; }
+    function store(data, previousResult) {
+        app[data.key] = data.value;
+        return $.Deferred().resolve(data).promise();
+    }
 
-			handler(action);
-		}
-	}
-}
+    var handlers = {rpc: rpc, store: store};
 
-function GenericTemplate(selector) {
-    var source = $(selector).html();
-    var template = Handlebars.compile(source);
+    function evaluate(action) {
+        var handler = handlers[action.type];
+        if (handler === undefined) {
+            console.log("Unknown action: ", action)
+            return;
+        }
+        
+        return handler(action, null);
+    }
+
+    function evaluateAll(actions) {
+        var promises = actions.map(evaluate);
+        $.when.apply($, promises).then(function() {
+
+        })
+    }
+
     return {
-        html: function(data) {
-            return template(data);
+        fire: function(name) {
+            var obj = actions[name];
+            if (obj !== undefined) {
+                return evaluateAll(obj);
+            }
         }
     }
 }
 
-function GenericCard(template, selector) {
-	var template = GenericTemplate(template);
-	var context = Context();
+function GenericCard(selector, options) {
+    var app = new Vue({
+        template: options.template,
+        data: options.data
+    });
 
-	return {
-		load: function(data) {
-			var loadFuncs = (data["actions"] || {})["$load"] || [];
-			loadFuncs.forEach(function(action) {
-				fireAction(action, context);
-			});
-			$(selector).append(template.html(data));
-		}
+    function mount() {
+        app.$mount();
+        var dom = app.$el;
+        $(selector).append(dom);
     }
+
+    return {
+        load: function(data) {
+            app.$actions = Actions(app, data["$actions"] || {});
+            delete data["$actions"]
+
+            Object.keys(data).forEach(function(key) {
+                app[key] = data[key];
+            });
+
+            app.$actions.fire("$load");
+            mount();
+        }
+    };
 }
 
-function SmallCard() {
-    return GenericTemplate("#template-small-card")
+function SmallCard(selector) {
+    return GenericCard(selector, {
+        template: "#template-small-card",
+        data: {
+            cardType: '',
+            icon: {},
+            title: '',
+            content: '',
+            footer: []
+        }
+    });
 }
 
 function MediumCard(selector) {
-    return GenericCard("#template-medium-card", selector);
+    return GenericCard(selector, {
+        template: "#template-medium-card",
+        data: {
+            cartTitle: '',
+            cardContent: [],
+            cardType: '',
+            icon: {},
+            title: '',
+            content: '',
+            footer: []
+        }
+    });
 }
