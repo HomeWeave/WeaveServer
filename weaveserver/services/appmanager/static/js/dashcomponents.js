93f5c9b0ef975aad29fbccd5f1a516236d7b6941
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-06-24 16:14:07 -0700

    Remove UUID4()

--- weaveserver/services/appmanager/static/js/dashcomponents.js
+++ weaveserver/services/appmanager/static/js/dashcomponents.js
@@ -1,11 +1,3 @@
-function uuidv4() {
-    // From: https://stackoverflow.com/a/2117523/227884
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
-
 function registerComponents() {
     Vue.component('card-footer-status', {
       template: '#template-footer-status',
