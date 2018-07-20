98ec0cc3df3748ef047d4b2974ff71925ee1ebed
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-07-19 22:47:28 -0700

    Add test plugin.

---
+++ test/services/plugins/test_dir/plugin1/testplugin/service.py
@@ -0,0 +1,23 @@
+from threading import Event
+
+from weavelib.rpc import RPCServer, ServerAPI
+from weavelib.services import BasePlugin
+
+
+class TestPluginService(BasePlugin):
+    def __init__(self, *args, **kwargs):
+        super(TestPluginService, self).__init__(*args, **kwargs)
+        self.rpc = RPCServer("test_plugin", "", [
+            ServerAPI("test", "", [], self.test)
+        ], self)
+        self.exited = Event()
+
+    def test(self):
+        return "test"
+
+    def on_service_start(self, *args, **kwargs):
+        self.notify_start()
+        self.exited.wait()
+
+    def on_service_stop(self):
+        self.exited.set()
