0ed9f8cf19fa996e5797ff6bf6f09e356dbfab8f
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-10-06 22:59:01 -0700

    Use a default template of empty string if provided template is null.

--- weaveserver/services/http/static/js/weave.js
+++ weaveserver/services/http/static/js/weave.js
@@ -226,7 +226,7 @@ function GenericApplication(selector, appData) {
         watch: watch,
         methods: {
             processUITemplate: function(template, item) {
-                return ExpressionEvaluator(app, {context: item})(template);
+                return ExpressionEvaluator(app, {context: item})(template || "");
             },
             evaluateCondition: function(condition) {
                 var res = ConditionEvaluator(app, {})(condition);
