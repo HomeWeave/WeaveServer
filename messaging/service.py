f18950ac6dc020cda0d4b302bfb40bdcf47b753a
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2019-04-04 13:09:38 -0700

    Get rid of old registry and weaveconnection objects.

--- messaging/service.py
+++ messaging/service.py
@@ -1,11 +1,9 @@
 from threading import Thread, Event
 
-from weavelib.messaging import WeaveConnection
 from weavelib.services import BasePlugin, MessagingEnabled
 
 from messaging.server import MessageServer
 from messaging.discovery import DiscoveryServer
-from messaging.appmgr import ApplicationRegistry
 
 
 PORT = 11023
@@ -26,8 +24,6 @@ class CoreService(BasePlugin, MessagingEnabled):
                                             self.message_server_started.set)
         self.message_server_thread = Thread(target=self.message_server.run)
         self.discovery_server = DiscoveryServer(PORT)
-        self.conn = WeaveConnection.local()
-        self.registry = ApplicationRegistry(self.conn, self)
 
     def before_service_start(self):
         """Need to override to prevent rpc_client connecting."""
@@ -35,14 +31,10 @@ class CoreService(BasePlugin, MessagingEnabled):
     def on_service_start(self, *args, **kwargs):
         self.message_server_thread.start()
         self.message_server_started.wait()
-        self.conn.connect()
-        self.registry.start()
         self.notify_start()
         self.shutdown_event.wait()
 
     def on_service_stop(self):
-        self.registry.stop()
-        self.conn.close()
         self.message_server.shutdown()
         self.message_server_thread.join()
         self.shutdown_event.set()
