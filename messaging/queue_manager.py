1dd72b0540febc34afc3804c8b4b17da8644a234
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2019-04-15 11:29:13 -0700

    Make way for future broadcast type topics.

--- messaging/queue_manager.py
+++ messaging/queue_manager.py
@@ -4,14 +4,14 @@ from jsonschema import Draft4Validator, SchemaError
 
 from weavelib.exceptions import ObjectNotFound, ObjectAlreadyExists
 from weavelib.exceptions import ObjectClosed, SchemaValidationFailed
-from weavelib.exceptions import InternalError
+from weavelib.exceptions import InternalError, BadArguments
 
 from .queues import FIFOQueue, SessionizedQueue
 
 
-class QueueInfo(object):
-    def __init__(self, queue_name, request_schema, response_schema,
-                 is_sessionized=False, force_auth=False):
+class ChannelInfo(object):
+    def __init__(self, channel_name, request_schema, response_schema,
+                 force_auth=False):
         try:
             Draft4Validator.check_schema(request_schema)
         except SchemaError:
@@ -21,36 +21,46 @@ class QueueInfo(object):
         except SchemaError:
             raise SchemaValidationFailed(response_schema)
 
-        self.queue_name = queue_name
+        self.channel_name = channel_name
         self.request_schema = request_schema
         self.response_schema = response_schema
-        self.queue_cls = FIFOQueue if not is_sessionized else SessionizedQueue
         self.force_auth = force_auth
+
+
+class QueueInfo(ChannelInfo):
+    def __init__(self, queue_name, request_schema, response_schema, queue_type,
+                 force_auth=True):
+        super(QueueInfo, self).__init__(queue_name, request_schema,
+                                        response_schema, force_auth=force_auth)
+        channel_map = {"fifo": FIFOQueue, "sessionized": SessionizedQueue}
+        self.queue_cls = channel_map.get(queue_type)
+        if not self.queue_cls:
+            raise BadArguments(queue_type)
 
     def create_queue(self):
         return self.queue_cls(self)
 
 
-class QueueRegistry(object):
+class ChannelRegistry(object):
     def __init__(self):
-        self.queue_map = {}
-        self.queue_map_lock = RLock()
+        self.channel_map = {}
+        self.channel_map_lock = RLock()
         self.active = True
 
     def create_queue(self, queue_name, request_schema, response_schema,
-                     is_sessionized=False, force_auth=False):
+                     queue_type, force_auth=False):
         queue_info = QueueInfo(queue_name, request_schema, response_schema,
-                               is_sessionized, force_auth)
+                               queue_type, force_auth)
 
-        with self.queue_map_lock:
+        with self.channel_map_lock:
             if not self.active:
                 raise ObjectClosed("Server shutting down.")
 
-            if queue_info.queue_name in self.queue_map:
-                raise ObjectAlreadyExists(queue_info.queue_name)
+            if queue_info.channel_name in self.channel_map:
+                raise ObjectAlreadyExists(queue_info.channel_name)
 
             queue = queue_info.create_queue()
-            self.queue_map[queue_info.queue_name] = queue
+            self.channel_map[queue_info.channel_name] = queue
 
         if not queue.connect():
             raise InternalError("Can't connect to queue: " + queue_name)
@@ -59,14 +69,14 @@ class QueueRegistry(object):
 
     def get_queue(self, queue_name):
         # TODO: Change to reader-writer lock.
-        with self.queue_map_lock:
+        with self.channel_map_lock:
             try:
-                return self.queue_map[queue_name]
+                return self.channel_map[queue_name]
             except KeyError:
                 raise ObjectNotFound(queue_name)
 
     def shutdown(self):
-        with self.queue_map_lock:
+        with self.channel_map_lock:
             self.active = False
-            for queue in self.queue_map.values():
-                queue.disconnect()
+            for channel in self.channel_map.values():
+                channel.disconnect()
--- test/test_queue_manager.py
+++ test/test_queue_manager.py
@@ -2,40 +2,46 @@ import pytest
 
 from weavelib.exceptions import SchemaValidationFailed, ObjectAlreadyExists
 from weavelib.exceptions import ObjectNotFound, InternalError, ObjectClosed
+from weavelib.exceptions import BadArguments
 
-from messaging.queue_manager import QueueRegistry
+from messaging.queue_manager import ChannelRegistry
 from messaging.queues import SessionizedQueue, FIFOQueue
 
 
-class TestQueueRegistry(object):
-    @pytest.mark.parametrize("is_sessionized,expected_cls",
-                             [(True, SessionizedQueue),
-                              (False, FIFOQueue)])
-    def test_create_queue_simple(self, is_sessionized, expected_cls):
-        registry = QueueRegistry()
-        queue = registry.create_queue("queue_name", {}, {}, is_sessionized)
+class TestChannelRegistry(object):
+    @pytest.mark.parametrize("queue_type,expected_cls",
+                             [("sessionized", SessionizedQueue),
+                              ("fifo", FIFOQueue)])
+    def test_create_queue_simple(self, queue_type, expected_cls):
+        registry = ChannelRegistry()
+        queue = registry.create_queue("queue_name", {}, {}, queue_type)
 
         assert isinstance(queue, expected_cls)
         assert registry.get_queue("queue_name") is queue
 
+    def test_create_queue_bad_queue_type(self):
+        registry = ChannelRegistry()
+        with pytest.raises(BadArguments):
+            registry.create_queue("queue_name", {}, {}, "bad-type")
+
     def test_create_queue_bad_schema(self):
-        registry = QueueRegistry()
+        registry = ChannelRegistry()
         with pytest.raises(SchemaValidationFailed):
-            registry.create_queue("queue_name", "test", {})
+            registry.create_queue("queue_name", "test", {}, "fifo")
 
         with pytest.raises(SchemaValidationFailed):
-            registry.create_queue("queue_name", {}, "test")
+            registry.create_queue("queue_name", {}, "test", "fifo")
 
     def test_queue_already_exists(self):
-        registry = QueueRegistry()
-        queue = registry.create_queue("queue_name", {}, {}, True)
+        registry = ChannelRegistry()
+        queue = registry.create_queue("queue_name", {}, {}, "sessionized")
         assert isinstance(queue, SessionizedQueue)
 
         with pytest.raises(ObjectAlreadyExists):
-            registry.create_queue("queue_name", {}, {}, False)
+            registry.create_queue("queue_name", {}, {}, "fifo")
 
     def test_get_queue_invalid(self):
-        registry = QueueRegistry()
+        registry = ChannelRegistry()
         with pytest.raises(ObjectNotFound):
             registry.get_queue("test_queue")
 
@@ -43,16 +49,16 @@ class TestQueueRegistry(object):
         backup = FIFOQueue.connect
         FIFOQueue.connect = lambda self: False
 
-        registry = QueueRegistry()
+        registry = ChannelRegistry()
         with pytest.raises(InternalError):
-            registry.create_queue("queue_name", {}, {}, False)
+            registry.create_queue("queue_name", {}, {}, "fifo")
 
         FIFOQueue.connect = backup
 
     def test_shutdown(self):
-        registry = QueueRegistry()
-        queue1 = registry.create_queue("queue1", {}, {}, is_sessionized=True)
-        queue2 = registry.create_queue("queue2", {}, {}, is_sessionized=False)
+        registry = ChannelRegistry()
+        queue1 = registry.create_queue("queue1", {}, {}, "fifo")
+        queue2 = registry.create_queue("queue2", {}, {}, "sessionized")
 
         flag = []
         def disconnect_fn():
@@ -66,4 +72,4 @@ class TestQueueRegistry(object):
         assert len(flag) == 2
 
         with pytest.raises(ObjectClosed):
-            registry.create_queue("queue3", {}, {})
+            registry.create_queue("queue3", {}, {}, "fifo")
