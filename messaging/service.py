c118c2b46cd4c947dbff9a739bfe7432f0414901
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2019-05-16 23:55:02 -0700

    Make CoreService inherit from BackgroundProcessServiceStart and BaseService instead of MessagingEnable. Less confusing hierarchy.

--- messaging/service.py
+++ messaging/service.py
@@ -2,7 +2,8 @@ from threading import Thread, Event
 from uuid import uuid4
 
 from weavelib.messaging import WeaveConnection
-from weavelib.services import BasePlugin, MessagingEnabled
+from weavelib.services import BackgroundProcessServiceStart, BaseService
+from weavelib.services import MessagingEnabled
 
 from messaging.server import MessageServer
 from messaging.discovery import DiscoveryServer
@@ -13,21 +14,26 @@ from messaging.appmgr import MessagingRPCHub
 
 PORT = 11023
 
+class DummyMessagingService(MessagingEnabled):
+    def __init__(self, auth_token, conn):
+        super(DummyMessagingService, self).__init__(auth_token=auth_token,
+                                                    conn=conn)
 
-class CoreService(MessagingEnabled, BasePlugin):
+    def start(self):
+        self.get_connection().connect()
+
+
+class CoreService(BackgroundProcessServiceStart, BaseService):
     def __init__(self, **kwargs):
-        messaging_token = "app-token-" + str(uuid4())
-        weave_env_token = kwargs.pop('auth_token')
-        conn = WeaveConnection.local()
-
-        kwargs['auth_token'] = messaging_token
-        kwargs['conn'] = conn
-
         super(CoreService, self).__init__(**kwargs)
 
+        messaging_token = "app-token-" + str(uuid4())
+        weave_env_token = kwargs.pop('auth_token')
+
+        self.dummy_service = DummyMessagingService(messaging_token,
+                                                   WeaveConnection.local())
         self.message_server_started = Event()
         self.shutdown_event = Event()
-
         app_registry = ApplicationRegistry([
             ("WeaveEnv", "http://github.com/HomeWeave/WeaveEnv.git",
              "app-id-weave-env", weave_env_token),
@@ -42,7 +48,8 @@ class CoreService(MessagingEnabled, BasePlugin):
         self.message_server_thread = Thread(target=self.message_server.run)
         self.discovery_server = DiscoveryServer(PORT)
         self.discovery_server_thread = Thread(target=self.discovery_server.run)
-        self.rpc_hub = MessagingRPCHub(self, channel_registry, app_registry)
+        self.rpc_hub = MessagingRPCHub(self.dummy_service, channel_registry,
+                                       app_registry)
 
     def before_service_start(self):
         """Need to override to prevent rpc_client connecting."""
@@ -51,12 +58,13 @@ class CoreService(MessagingEnabled, BasePlugin):
         self.message_server_thread.start()
         self.message_server_started.wait()
         self.discovery_server_thread.start()
-        self.get_connection().connect()
+        self.dummy_service.get_connection().connect()
         self.rpc_hub.start()
         self.notify_start()
         self.shutdown_event.wait()
 
     def on_service_stop(self):
+        self.dummy_service.get_connection().close()
         self.message_server.shutdown()
         self.message_server_thread.join()
         self.shutdown_event.set()
